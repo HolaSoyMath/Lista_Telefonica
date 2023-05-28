ceb7352b08161977d84058daad56fc5c0657113a
Matheus Guimarães <theo.guimaraes@hotmail.com>
2023-05-28 13:33:46 -0300

    Lista Telefonica em C# console

---
+++ Lista_Telefonica/Lista_Telefonica/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+
+// Classe ComparadorNome para comparar os valores na coluna de nomes
+public class ComparadorNome : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        string nome1 = ((string[,])x)[((string[,])x).GetLowerBound(0), 1];
+        string nome2 = ((string[,])y)[((string[,])y).GetLowerBound(0), 1];
+
+        return string.Compare(nome1, nome2);
+    }
+}
+
+class ListaTelefonica
+{
+    public static string[,] listaTel = new string[9999, 3];
+    public static int ID = 0;
+    static void Main()
+    {
+        listaTel[0, 0] = "0";
+        listaTel[0, 1] = "Math";
+        listaTel[0, 2] = "9988";
+
+        listaTel[1, 0] = "1";
+        listaTel[1, 1] = "Bernardo";
+        listaTel[1, 2] = "1452";
+
+        listaTel[2, 0] = "2";
+        listaTel[2, 1] = "Murilo";
+        listaTel[2, 2] = "7754";
+
+        ID = 3;
+
+
+
+        while (true)
+        {
+            menu();
+        }
+
+    }
+
+    static void menu()
+    {
+        Console.WriteLine("Lista Telefônica");
+        Console.WriteLine("1. Adicionar Contato");
+        Console.WriteLine("2. Buscar Contato por Telefone");
+        Console.WriteLine("3. Buscar Contato por nome");
+        Console.WriteLine("4. Excluir Contato Completo por Telefone");
+        Console.WriteLine("5. Excluir Contato Completo por Nome");
+        Console.WriteLine("6. Ordenar Contatos por Telefone");
+        Console.WriteLine("7. Ordenar Contatos por Nome");
+        Console.WriteLine("8. Lista Todos os Contatos");
+
+        int escolha = int.Parse(Console.ReadLine());
+
+        escolheAcao(escolha);
+    }
+    static void escolheAcao(int escolha)
+    {
+
+        switch (escolha)
+        {
+            case 1:
+                lerInformacoes();
+                break;
+            case 2:
+                buscarContatoTelefone();
+                break;
+            case 3:
+                buscarContatoNome();
+                break;
+            case 4:
+                excluirContatoTelefone();
+                break;
+            case 5:
+                excluirContatoNome();
+                break;
+            case 6:
+                ordenarContatoTelefone();
+                break;
+            case 7:
+                ordenarContatoNome();
+                break;
+            case 8:
+                listarListaTel();
+                break;
+        }
+    }
+
+    static void listarListaTel()
+    {
+        for (int i = 0; i < ID; i++)
+        {
+            if (listaTel[i, 0] != null)
+            {
+                mostrarRegistro(i);
+            }
+        }
+        Console.WriteLine("-------------------------------------");
+    }
+
+    static void excluirRegistro(int i)
+    {
+        listaTel[i, 0] = " ";
+        listaTel[i, 1] = " ";
+        listaTel[i, 2] = " ";
+    }
+
+    static void excluirContatoNome()
+    {
+        Console.WriteLine("Digite um nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("-------------------------------------");
+
+        for (int i = 0; i < listaTel.GetLength(0); i++)
+        {
+            if (listaTel[i, 1] == nome)
+            {
+                excluirRegistro(i);
+                Console.WriteLine("Contato Excluído!");
+            }
+        }
+        Console.WriteLine("-------------------------------------");
+    }
+
+    static void excluirContatoTelefone()
+    {
+        Console.WriteLine("Digite um número de telefone:");
+        uint telefone = uint.Parse(Console.ReadLine());
+        Console.WriteLine("-------------------------------------");
+
+        for (int i = 0; i < listaTel.GetLength(0); i++)
+        {
+            if (uint.TryParse(listaTel[i, 2], out uint numero) && numero == telefone)
+            {
+                excluirRegistro(i);
+            }
+        }
+        Console.WriteLine("-------------------------------------");
+    }
+
+    static void mostrarRegistro(int i)
+    {
+        Console.Write("ID: " + listaTel[i, 0]);
+        Console.Write(" | Nome: " + listaTel[i, 1]);
+        Console.WriteLine(" | Telefone: " + listaTel[i, 2]);
+    }
+
+    static void buscarContatoNome()
+    {
+        Console.WriteLine("Digite um nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("-------------------------------------");
+
+        for (int i = 0; i < listaTel.GetLength(0); i++)
+        {
+            if (listaTel[i, 1] == nome)
+            {
+                mostrarRegistro(i);
+            }
+        }
+        Console.WriteLine("-------------------------------------");
+    }
+
+    static void buscarContatoTelefone()
+    {
+        Console.WriteLine("Digite um número de telefone:");
+        uint telefone = uint.Parse(Console.ReadLine());
+        Console.WriteLine("-------------------------------------");
+
+        for (int i = 0; i < listaTel.GetLength(0); i++)
+        {
+            if (uint.TryParse(listaTel[i, 2], out uint numero) && numero == telefone)
+            {
+                mostrarRegistro(i);
+            }
+        }
+        Console.WriteLine("-------------------------------------");
+    }
+
+    static void adicionarContato(string nome, uint telefone)
+    {
+        listaTel[ID, 0] = ID.ToString();
+        listaTel[ID, 1] = nome;
+        listaTel[ID, 2] = telefone.ToString();
+        ID++;
+    }
+
+    static void lerInformacoes()
+    {
+        uint telefone;
+        string nome;
+
+        Console.WriteLine("Insira um Nome:");
+        nome = Console.ReadLine();
+        Console.WriteLine("Insira um Telefone:");
+        telefone = uint.Parse(Console.ReadLine());
+        adicionarContato(nome, telefone);
+        Console.WriteLine("-------------------------------------");
+    }
+
+    static void ordenarContatoNome()
+    {
+        for (int i = 0; i < ID - 1; i++)
+        {
+            for (int j = 0; j < ID - i - 1; j++)
+            {
+                string nome1 = listaTel[j, 1];
+                string nome2 = listaTel[j + 1, 1];
+
+                if (string.Compare(nome1, nome2) > 0)
+                {
+                    // Trocar os elementos de posição
+                    string tempID = listaTel[j, 0];
+                    string tempNome = listaTel[j, 1];
+                    string tempTelefone = listaTel[j, 2];
+
+                    listaTel[j, 0] = listaTel[j + 1, 0];
+                    listaTel[j, 1] = listaTel[j + 1, 1];
+                    listaTel[j, 2] = listaTel[j + 1, 2];
+
+                    listaTel[j + 1, 0] = tempID;
+                    listaTel[j + 1, 1] = tempNome;
+                    listaTel[j + 1, 2] = tempTelefone;
+                }
+            }
+        }
+        listarListaTel();
+    }
+
+    static void ordenarContatoTelefone()
+    {
+        for (int i = 0; i < ID - 1; i++)
+        {
+            for (int j = 0; j < ID - i - 1; j++)
+            {
+                uint telefone1, telefone2;
+
+                if (uint.TryParse(listaTel[j, 2], out telefone1) && uint.TryParse(listaTel[j + 1, 2], out telefone2))
+                {
+                    if (telefone1 > telefone2)
+                    {
+                        // Trocar os elementos de posição
+                        string tempID = listaTel[j, 0];
+                        string tempNome = listaTel[j, 1];
+                        string tempTelefone = listaTel[j, 2];
+
+                        listaTel[j, 0] = listaTel[j + 1, 0];
+                        listaTel[j, 1] = listaTel[j + 1, 1];
+                        listaTel[j, 2] = listaTel[j + 1, 2];
+
+                        listaTel[j + 1, 0] = tempID;
+                        listaTel[j + 1, 1] = tempNome;
+                        listaTel[j + 1, 2] = tempTelefone;
+                    }
+                }
+            }
+        }
+        listarListaTel();
+    }
+
+}
